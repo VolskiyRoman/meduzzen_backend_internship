bfc92269f3c2a8848575966a400954ef816d5dc8
Roma <romanvolskiy791@gmail.com>
2023-09-27 03:37:03 +0300

    Added user CRUD

--- quizes/urls.py
+++ quizes/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from users.views import UserViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("health_check.urls")),  # Use your app's name "health_check"
+    path('api/', include(router.urls)),
+    path('create_user/', UserViewSet.as_view({'get': 'create_user_form'}), name='create_user_form'),
+    path("", include("health_check.urls")),
 ]
