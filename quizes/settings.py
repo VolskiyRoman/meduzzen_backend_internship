1c4273522b1eb613e97f19c524581331dff8ba05
Roma <romanvolskiy791@gmail.com>
2023-10-11 22:43:43 +0300

    added secrets

--- quizes/settings.py
+++ quizes/settings.py
@@ -78,9 +78,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'quizes.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
